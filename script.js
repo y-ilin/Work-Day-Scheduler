d69fff1e6601e72695377f61e60197388c445e71
Yi Lin Lee <yilinlee19@gmail.com>
2020-06-13 22:38:41 +1000

    clean up comments

--- script.js
+++ script.js
@@ -65,13 +65,13 @@ $(document).ready(function () {
     var handleSaveBtn = function(e) {
         if( $(e.target).hasClass("saveBtn")) {
             var newEvent = $(e.currentTarget).find(".eventArea").val();
-            // NOW SAVE TO LOCAL STORAGEEEE
             var newEventDate = now.format("YYYY-MM-DD") + "-" + e.currentTarget.getAttribute("id");
             eventsObj[newEventDate] = newEvent;
             localStorage.setItem("eventsString", JSON.stringify(eventsObj));
         }
     }
 
+    // Listen for click event on save buttons
     $(".row").on("click", handleSaveBtn);
 
     // Create button for clearing the day's schedule
@@ -92,20 +92,3 @@ $(document).ready(function () {
         localStorage.setItem("eventsString", JSON.stringify(eventsObj));
     });
 });
-
-
-`
-GIVEN I am using a daily planner to create a schedule
-WHEN I open the planner
-THEN the current day is displayed at the top of the calendar
-WHEN I scroll down
-THEN I am presented with timeblocks for standard business hours
-WHEN I view the timeblocks for that day
-THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-WHEN I click into a timeblock
-THEN I can enter an event
-WHEN I click the save button for that timeblock
-THEN the text for that event is saved in local storage
-WHEN I refresh the page
-THEN the saved events persist
-`
